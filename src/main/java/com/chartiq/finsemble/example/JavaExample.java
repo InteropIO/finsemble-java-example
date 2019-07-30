2512aa08304ea471032b9edb778d61fc51ddc4f4
NinjaManatee <andy@chartiq.com>
2019-07-30 13:51:11 -0400

    Renaming JavaExample2 to JavaExample

---
+++ src/main/java/com/chartiq/finsemble/example/JavaExample.java
@@ -0,0 +1,459 @@
+package com.chartiq.finsemble.example;
+
+import com.chartiq.finsemble.Finsemble;
+import com.chartiq.finsemble.interfaces.ConnectionEventGenerator;
+import com.chartiq.finsemble.interfaces.ConnectionListener;
+
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class JavaExample {
+    /**
+     * Logger
+     */
+    private static final Logger LOGGER = Logger.getLogger(JavaExample.class.getName());
+
+    private JPanel mainPanel;
+    private JButton sendSymbolButton;
+    private JTextField symbolTextField;
+    private JComboBox componentComboBox;
+    private JButton launchComponentButton;
+    private JCheckBox dockCheckBox;
+    private JCheckBox group1CheckBox;
+    private JCheckBox group2CheckBox;
+    private JCheckBox group3CheckBox;
+    private JCheckBox group4CheckBox;
+    private JCheckBox group5CheckBox;
+    private JCheckBox group6CheckBox;
+    private JButton messagesButton;
+    private JTextArea messages;
+
+    private final Finsemble fsbl;
+
+    /**
+     * Initializes a new instance of the JavaExample class.
+     *
+     * @param args
+     */
+    public JavaExample(List<String> args) {
+        appendMessage(String.format("Finsemble Java Example starting with arguments:\n\t%s", String.join("\n\t", args)));
+        appendMessage("Initiating Finsemble connection");
+
+        // Get frame for registration with Finsemble
+        JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(mainPanel);
+
+        // TODO: populate this with a way to test the API
+        fsbl = new Finsemble(args, frame);
+        try {
+            fsbl.connect();
+            appendMessage("Connected to Finsemble");
+
+            fsbl.addListener(new ConnectionListener() {
+                @Override
+                public void disconnected(ConnectionEventGenerator from) {
+                    LOGGER.info("Finsemble connection closed");
+                }
+
+                @Override
+                public void error(ConnectionEventGenerator from, Exception e) {
+                    LOGGER.log(Level.SEVERE, "Error from Finsemble", e);
+                }
+            });
+            fsbl.register();
+            appendMessage("Window registered with Finsemble");
+
+            initForm();
+            setFormEnable(true);
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, "Error initializing Finsemble connection", ex);
+            appendMessage("Error initializing Finsemble connection: " + ex.getMessage());
+            try {
+                fsbl.close();
+            } catch (IOException e1) {
+                LOGGER.log(Level.SEVERE, "Error closing Finsemble connection", e1);
+                appendMessage((e1.getMessage()));
+            }
+        }
+    }
+
+    /**
+     * Toggles the visibility of the messages panel.
+     */
+    private void toggleMessages() {
+        final boolean isVisible = messages.isVisible();
+        messages.setVisible(!isVisible);
+    }
+
+    private void initForm() {
+        // Add click handlers
+
+        // Send symbol
+
+        // populate component combobox
+        fsbl.getClients().getLauncherClient().getComponentList((err, res) -> {
+            if (err != null) {
+                LOGGER.log(Level.SEVERE, "Error getting component list", err);
+            } else {
+                //
+                LOGGER.info("Break here");
+                ;
+            }
+        });
+
+        // Add launch component button handler
+        launchComponentButton.addActionListener((e) -> launchComponent());
+
+        // dock checkbox
+
+        // group check boxes
+
+        // messages button
+        messagesButton.addActionListener((e) -> this.toggleMessages());
+    }
+
+    /**
+     * Launches the currently selected component from the component combo box.
+     */
+    private void launchComponent() {
+        final String componentName = componentComboBox.getSelectedItem().toString();
+        fsbl.getClients().getLauncherClient().spawn(componentName, null, (err, res) -> {
+            if (err != null) {
+                LOGGER.log(Level.SEVERE, String.format("Error spawning \"%s\"", componentName), err);
+            } else {
+                LOGGER.info(String.format("\"%s\" spawned", componentName));
+            }
+        });
+    }
+
+
+    private void setFormEnable(boolean enabled) {
+        symbolTextField.setEnabled(enabled);
+        componentComboBox.setEnabled(enabled);
+        launchComponentButton.setEnabled(enabled);
+        dockCheckBox.setEnabled(enabled);
+        group1CheckBox.setEnabled(enabled);
+        group2CheckBox.setEnabled(enabled);
+        group3CheckBox.setEnabled(enabled);
+        group4CheckBox.setEnabled(enabled);
+        group5CheckBox.setEnabled(enabled);
+        group6CheckBox.setEnabled(enabled);
+    }
+
+    /**
+     * Adds a message to the message box.
+     *
+     * @param s The message to add.
+     */
+    private void appendMessage(String s) {
+        LOGGER.info(String.format("Message to UI", s));
+        try {
+            Document doc = messages.getDocument();
+            doc.insertString(0, String.format("%s\n", s), null);
+        } catch (BadLocationException exc) {
+            LOGGER.severe(exc.getMessage());
+        }
+    }
+
+    /**
+     * The main function of the application.
+     *
+     * @param args The command line arguments.
+     */
+    public static void main(String[] args) {
+        final List<String> argList = new ArrayList<>(Arrays.asList(args));
+
+        initLogging(argList);
+
+        // Parse message into ArrayList of strings so arguments can be added.
+        final List<String> otherArgs = new ArrayList<>(Arrays.asList(args));
+
+        otherArgs.add("Not first");
+        launchForm(otherArgs);
+
+        argList.add("First");
+        launchForm(argList);
+
+        // the following statement is used to log any messages
+        LOGGER.info(String.format("Starting JavaExample: %s", Arrays.toString(args)));
+    }
+
+    private static void initLogging(List<String> args) {
+        if (System.getProperty("java.util.logging.config.file") != null) {
+            // Config file property has been set, no further initialization needed.
+            return;
+        }
+
+        // Check whether logging file has been specified
+        final List<String> properties = args
+                .stream()
+                .filter(arg -> arg.startsWith("-Djava.util.logging.config.file"))
+                .collect(Collectors.toList());
+
+        if ((properties.size() == 0) || !properties.get(0).contains("=")) {
+            // No logging properties specified
+            return;
+        }
+
+        // Get filename from parameter
+        final String loggingPropertiesPath = properties.get(0).split("=")[1];
+        try {
+            final InputStream inputStream = new FileInputStream(loggingPropertiesPath);
+            LogManager.getLogManager().readConfiguration(inputStream);
+        } catch (final IOException e) {
+            LOGGER.log(Level.SEVERE, "Could not load default logging.properties file", e);
+        }
+    }
+
+    /**
+     * Launches the example form based on the command line arguments.
+     *
+     * @param args The command line arguments.
+     */
+    private static void launchForm(List<String> args) {
+        final JFrame frame = new JFrame("JavaExample");
+        frame.setContentPane(new JavaExample(args).mainPanel);
+        frame.pack();
+        frame.setSize(800, 400);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
+        componentComboBox = new JComboBox();
+        GridBagConstraints gbc;
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        mainPanel.add(componentComboBox, gbc);
+        launchComponentButton = new JButton();
+        launchComponentButton.setEnabled(false);
+        launchComponentButton.setText("Launch Component");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        mainPanel.add(launchComponentButton, gbc);
+        final JLabel label1 = new JLabel();
+        Font label1Font = this.$$$getFont$$$(null, -1, 48, label1.getFont());
+        if (label1Font != null) label1.setFont(label1Font);
+        label1.setHorizontalAlignment(2);
+        label1.setHorizontalTextPosition(11);
+        label1.setInheritsPopupMenu(false);
+        label1.setText("AAPL");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        mainPanel.add(label1, gbc);
+        dockCheckBox = new JCheckBox();
+        dockCheckBox.setEnabled(true);
+        dockCheckBox.setText("Dock");
+        dockCheckBox.setVisible(false);
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        mainPanel.add(dockCheckBox, gbc);
+        symbolTextField = new JTextField();
+        symbolTextField.setEditable(false);
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        mainPanel.add(symbolTextField, gbc);
+        sendSymbolButton = new JButton();
+        sendSymbolButton.setEnabled(false);
+        sendSymbolButton.setText("Send Symbol");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(sendSymbolButton, gbc);
+        group1CheckBox = new JCheckBox();
+        group1CheckBox.setEnabled(false);
+        group1CheckBox.setText("Group 1");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 5;
+        gbc.anchor = GridBagConstraints.WEST;
+        mainPanel.add(group1CheckBox, gbc);
+        group2CheckBox = new JCheckBox();
+        group2CheckBox.setEnabled(false);
+        group2CheckBox.setText("Group 2");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 6;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        mainPanel.add(group2CheckBox, gbc);
+        group3CheckBox = new JCheckBox();
+        group3CheckBox.setEnabled(false);
+        group3CheckBox.setText("Group 3");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 7;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        mainPanel.add(group3CheckBox, gbc);
+        group4CheckBox = new JCheckBox();
+        group4CheckBox.setEnabled(false);
+        group4CheckBox.setText("Group 4");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 8;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        mainPanel.add(group4CheckBox, gbc);
+        group5CheckBox = new JCheckBox();
+        group5CheckBox.setEnabled(false);
+        group5CheckBox.setText("Group 5");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 9;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        mainPanel.add(group5CheckBox, gbc);
+        group6CheckBox = new JCheckBox();
+        group6CheckBox.setEnabled(false);
+        group6CheckBox.setText("Group 6");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 10;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        mainPanel.add(group6CheckBox, gbc);
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridBagLayout());
+        panel1.setBackground(new Color(-7896643));
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(panel1, gbc);
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridBagLayout());
+        panel2.setBackground(new Color(-8139));
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(panel2, gbc);
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridBagLayout());
+        panel3.setBackground(new Color(-7743485));
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(panel3, gbc);
+        final JPanel panel4 = new JPanel();
+        panel4.setLayout(new GridBagLayout());
+        panel4.setBackground(new Color(-105886));
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(panel4, gbc);
+        final JPanel panel5 = new JPanel();
+        panel5.setLayout(new GridBagLayout());
+        panel5.setBackground(new Color(-13783809));
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 9;
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(panel5, gbc);
+        final JPanel panel6 = new JPanel();
+        panel6.setLayout(new GridBagLayout());
+        panel6.setBackground(new Color(-24064));
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 10;
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(panel6, gbc);
+        final JPanel spacer1 = new JPanel();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 11;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        mainPanel.add(spacer1, gbc);
+        messagesButton = new JButton();
+        messagesButton.setEnabled(true);
+        messagesButton.setFocusable(false);
+        messagesButton.setText("Messages");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 12;
+        gbc.gridwidth = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        mainPanel.add(messagesButton, gbc);
+        messages = new JTextArea();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 13;
+        gbc.gridwidth = 3;
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(messages, gbc);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    private Font $$$getFont$$$(String fontName, int style, int size, Font currentFont) {
+        if (currentFont == null) return null;
+        String resultName;
+        if (fontName == null) {
+            resultName = currentFont.getName();
+        } else {
+            Font testFont = new Font(fontName, Font.PLAIN, 10);
+            if (testFont.canDisplay('a') && testFont.canDisplay('1')) {
+                resultName = fontName;
+            } else {
+                resultName = currentFont.getName();
+            }
+        }
+        return new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return mainPanel;
+    }
+
+}
