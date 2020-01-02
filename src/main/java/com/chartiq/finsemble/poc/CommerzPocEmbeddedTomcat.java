716e2097dbdba513bb8cbd79e7a23bae23bf037d
Ethan Leung <ethan@chartiq.com>
2020-01-02 17:57:47 +0800

    Added embedded tomcat example and build a separate war for embedded tomcat

---
+++ src/main/java/com/chartiq/finsemble/poc/CommerzPocEmbeddedTomcat.java
@@ -0,0 +1,119 @@
+package com.chartiq.finsemble.poc;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.chartiq.finsemble.Finsemble;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.jasper.servlet.JspServlet;
+import org.json.JSONObject;
+
+
+public class CommerzPocEmbeddedTomcat {
+    public static void main(String[] args) throws LifecycleException {
+        final List<String> argList = new ArrayList<>(Arrays.asList(args));
+        final Finsemble fsbl = new Finsemble(argList);
+
+        //Create Tomcat
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("tomcat");
+        tomcat.setPort(8080);
+
+        //Add war
+        String contextPath = "/CommerzPocWebapp";
+        String warFilePath = new File("CommerzPocWebappEmbedded.war").getAbsolutePath();
+        tomcat.getHost().setAppBase(".");
+        tomcat.addWebapp(contextPath, warFilePath);
+
+        //Add servlet
+        String servletContextPath = "/CommerzPocWebapp/CommerzPocServlet";
+        String docBase = new File(".").getAbsolutePath();
+        Context context = tomcat.addContext(servletContextPath, docBase);
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                response.setHeader("Access-Control-Allow-Origin","http://localhost:3375");
+                final PrintWriter out = response.getWriter();
+                final String action = request.getParameter("action");
+                switch (action) {
+                    case "getComponentList":
+                        fsbl.getClients().getLauncherClient().getComponentList((err, res) -> out.print(res));
+                        break;
+                    case "spawn":
+                        final String componentName = request.getParameter("componentName");
+                        fsbl.getClients().getLauncherClient().spawn(componentName, new JSONObject() {{
+                            put("addToWorkspace", true);
+                        }}, (err, res) -> {
+                            if (err != null) {
+                                fsbl.getClients().getLogger().error(String.format("Error spawning \"%s\"", componentName));
+                                out.println(err.toString());
+                            } else {
+                                fsbl.getClients().getLogger().info(String.format("\"%s\" spawned by Tomcat", componentName));
+                                out.print(res);
+                            }
+                        });
+                        break;
+                    case "stopEmbeddedTomcat":
+                        try {
+                            fsbl.getClients().getLogger().log("Tomcat closed!!!");
+                            fsbl.close();
+                            out.print("{'status':'closed'}");
+                            out.flush();
+                            tomcat.stop();
+                            tomcat.destroy();
+                        } catch (LifecycleException e) {
+                            e.printStackTrace();
+                        }finally {
+                            System.exit(0);
+                        }
+
+                        break;
+                    default:
+                        out.println("{'status':'ok'}");
+                        break;
+                }
+                out.flush();
+            }
+        };
+
+        String servletName = "CommerzPocServlet";
+        String urlPattern = "/do";
+
+        tomcat.addServlet(servletContextPath, servletName, servlet);
+        context.addServletMappingDecoded(urlPattern, servletName);
+
+        tomcat.start();
+
+        //Connect FSBL
+        try {
+            fsbl.connect();
+            fsbl.getClients().getLogger().log("Tomcat connected!!!");
+
+            final String componentName = "commerz_poc_jsp";
+            fsbl.getClients().getLauncherClient().spawn(componentName, new JSONObject() {{
+                put("addToWorkspace", true);
+            }}, (err, res) -> {
+                if (err != null) {
+                    fsbl.getClients().getLogger().error(String.format("Error spawning \"%s\"", componentName));
+                } else {
+                    fsbl.getClients().getLogger().info(String.format("\"%s\" spawned by Tomcat", componentName));
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        tomcat.getServer().await();
+    }
+}
---
+++ src/main/webapp_embedded/index.js
@@ -0,0 +1,92 @@
+const FSBLReady = () => {
+	try {
+		// Subscribe to router message
+		FSBL.Clients.RouterClient.addListener('color', onRouterValueChanged)
+		restoreState()
+	} catch (e) {
+		FSBL.Clients.Logger.error(e);
+	}
+}
+
+function onRouterValueChanged(err, response) {
+	if (!err) {
+		if (response.data) {
+			changeColor(response.data.r, response.data.g, response.data.b)
+		}
+	}
+}
+
+function changeColor(r, g, b) {
+	document.getElementById('color').style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+	saveState(r, g, b)
+}
+
+function saveState(r, g, b) {
+	const params = {
+		fields: [{
+				field: "r",
+				value: r
+			},
+			{
+				field: "g",
+				value: g
+			},
+			{
+				field: "b",
+				value: b
+			}
+		]
+	}
+	FSBL.Clients.WindowClient.setComponentState(params);
+}
+
+function restoreState(){
+	const params = {
+		fields: [
+			"r",
+			"g",
+			"b"
+		]
+	};
+
+	// TODO: Change to await so show can be after restore
+	FSBL.Clients.WindowClient.getComponentState(params, componentStateHandler);
+}
+
+function componentStateHandler(err, state){
+	changeColor(state.r, state.g, state.b)
+}
+
+function post_to_url(path, params) {
+    for(var key in params) {
+        if(path.includes("?"))
+            path+="&"+key+"="+params[key]
+        else
+            path+="?"+key+"="+params[key]
+    }
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("POST", path, true);
+    xhttp.send();
+}
+
+function stopEmbeddedTomcat(){
+    post_to_url("CommerzPocServlet/do",{action:'stopEmbeddedTomcat'})
+}
+function spawnSwing(){
+    post_to_url("CommerzPocServlet/do",{action:'spawn',componentName:"commerz_poc_swing"})
+}
+
+function spawnHtml(){
+    post_to_url("CommerzPocServlet/do",{action:'spawn',componentName:"commerz_poc_html"})
+}
+
+function getComponentList(){
+    post_to_url("CommerzPocServlet/do",{action:'getComponentList'})
+}
+
+if (window.FSBL && FSBL.addEventListener) {
+	FSBL.addEventListener("onReady", FSBLReady)
+} else {
+	window.addEventListener("FSBLReady", FSBLReady)
+}
