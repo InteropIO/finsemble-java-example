63493d06350b42e162b4b8722752a631cfc9baef
Tiago Pina <tiago.pina@cosaic.io>
2021-05-05 09:52:48 +0100

    Updated delimiter

--- src/main/java/com/chartiq/finsemble/example/MultiWindowJavaSwingExample.java
+++ src/main/java/com/chartiq/finsemble/example/MultiWindowJavaSwingExample.java
@@ -11,7 +11,7 @@ public class MultiWindowJavaSwingExample {
     // JUnique has a bug in which if you pass a non-unicode char, the recreation of the string from
     // a inputstream gets messed up - therefore we have chosen to use & has the delimiter
     // similar to what gets used in a query params URL
-    private static final String DELIMITER = "&";
+    private static final String DELIMITER = "!&!";
 
     public static void main(String[] args) {
 
